b80dec8ca2158e5f3214c698d95869c9ead27f3c
adibzikri <adibzikri0819@gmail.com>
2023-03-28 14:10:35 +0800

    first commit

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge:[],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
